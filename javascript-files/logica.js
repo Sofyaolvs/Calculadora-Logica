769264d51f1c6aeb8f59f14aba3a34dc119541a2
João Igor <joaoigor21052004@gmail.com>
2024-08-11 00:22:50 -0300

    +

--- javascript-files/logica.js
+++ javascript-files/logica.js
@@ -1,3 +1,7 @@
 import { proposicao } from "./funcoes";
 
 const hierarquia = ["(", ")", "~", "^", "v", "→", "↔"];
+
+const A = [true, true, true, true, false, false, false, false] //Contém 8 possibilidades
+const B = [true, true, false, false, true, false, true, false] //Contém 8 possibilidades
+const C = [true, false, true, false, true, true, false, false] //Contém 8 possibilidades
