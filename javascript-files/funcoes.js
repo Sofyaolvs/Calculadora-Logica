57a9ecb291d3bcb9f5e5462abb17a1d7fa293b34
Sofya <146602186+Sofyaolvs@users.noreply.github.com>
2024-08-11 11:45:40 -0300

    Update funcoes.js

--- javascript-files/funcoes.js
+++ javascript-files/funcoes.js
@@ -1,4 +1,11 @@
+// Variavel Global Display
 let proposicao = "";
+
+// Adiciona letra/caractere na proposição
+function addCaractere(caract) {
+    proposicao += caract;
+    atualizarProp();
+}
 
 // Função para deletar o último caractere ou limpar a proposição
 function deleteProp(text) {
@@ -15,9 +22,17 @@ function atualizarProp() {
     document.getElementById('display').value = proposicao;
 }
 
-function addCaractere(caract) {
-    proposicao += caract; // Adiciona letra/caractere na proposição
-    atualizarProp();
-}
+function gerarTabelaVerdade(proposicao) {
+    //pega todas as variaveis 
+    const variaveis = Array.from(new Set(proposicao.replace(/[^A-Z]/g, '')));
+    const linhas = Math.pow(2, variaveis.length);  //calcula o numero de linhas
 
-export { proposicao, deleteProp, addCaractere }
+    let tabelaHtml = '<table class="table"><tr>'; //cria uma string p armazenar o conteudo ,
+    //cria <table> vai ser container da tabela html e 
+    //atribuiu a classe table por causa do css
+    variaveis.forEach(v => tabelaHtml += `<th>${v}</th>`);//itera o array variaveis e pra cada variavel "v"
+    // add um 
+    tabelaHtml += `<th>${proposicao}</th></tr>`; //cabeçalho das colunas e adc a ultima celula de cabeçalho(linha)
+
+}
+export { proposicao, deleteProp, addCaractere, gerarTabelaVerdade }
