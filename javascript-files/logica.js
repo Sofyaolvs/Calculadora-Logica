666956d59f7fc851d0639d4968b0c4592a731266
João Igor <joaoigor21052004@gmail.com>
2024-08-11 00:43:00 -0300

    +

--- javascript-files/logica.js
+++ javascript-files/logica.js
@@ -5,3 +5,5 @@ const hierarquia = ["(", ")", "~", "^", "v", "→", "↔"];
 const A = [true, true, true, true, false, false, false, false] //Contém 8 possibilidades
 const B = [true, true, false, false, true, false, true, false] //Contém 8 possibilidades
 const C = [true, false, true, false, true, true, false, false] //Contém 8 possibilidades
+
+let operacoesLogicas = []
