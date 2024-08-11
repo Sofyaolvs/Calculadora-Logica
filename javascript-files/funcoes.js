b426a6405cbf777ab392de415c1b84fceaa1cd0e
João Igor <joaoigor21052004@gmail.com>
2024-08-10 23:07:09 -0300

    Algumas Pendencias

---
+++ javascript-files/funcoes.js
@@ -0,0 +1,23 @@
+let proposicao = "";
+
+// Função para deletar o último caractere ou limpar a proposição
+function deleteProp(text) {
+    if (text === "⌫") {
+        proposicao = proposicao.slice(0, -1);
+    } else {
+        proposicao = "";
+    }
+    atualizarProp();
+}
+
+// Função para atualizar o display 
+function atualizarProp() {
+    document.getElementById('display').value = proposicao;
+}
+
+function addCaractere(caract) {
+    proposicao += caract; // Adiciona letra/caractere na proposição
+    atualizarProp();
+}
+
+export {proposicao, deleteProp, addCaractere}
