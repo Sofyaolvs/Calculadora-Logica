64a9817d02084c2e2c1f643d4f4ea74d07d30f34
Sofya <146602186+Sofyaolvs@users.noreply.github.com>
2024-08-12 16:31:00 -0300

    Update script.js

--- javascript-files/script.js
+++ javascript-files/script.js
@@ -1,18 +1,15 @@
 import { addCaractere, deleteProp,gerarTabelaVerdade,proposicao} from './funcoes.js';
 
-// Adiciona eventos de clique aos botões de caracteres
 document.querySelectorAll('.caract').forEach(function (button) {
     button.addEventListener('click', function () {
         addCaractere(button.textContent);
     });
 });
 
-// Evento p o botão de limpar
 document.querySelector(".clear-button").addEventListener("click", function () {
     deleteProp("C");
 });
 
-// Evento p o botão de backspace
 document.querySelector(".back").addEventListener("click", function () {
     deleteProp("⌫");
 });
@@ -20,7 +17,6 @@ document.querySelector(".back").addEventListener("click", function () {
 document.querySelector(".equal").addEventListener("click", function() {
     gerarTabelaVerdade(proposicao); // Gera a tabela verdade baseada na proposição
 });
-
 
 
 
